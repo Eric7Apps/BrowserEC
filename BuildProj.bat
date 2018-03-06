3735b78cb664b21841f57739ddb10e56c8186c27
Eric Chauvin <ericsourcecode@gmail.com>
2018-03-06 10:48:05 -0700

    Create BuildProj.bat

---
+++ BuildProj.bat
@@ -0,0 +1,6 @@
+
+rem MSBuild Reference:
+rem https://docs.microsoft.com/en-us/visualstudio/msbuild/msbuild-reference
+
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe  /nologo /fileLogger BrowserEC2.csproj
+pause
